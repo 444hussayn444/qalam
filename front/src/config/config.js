810f8235d4199ffb226233b246a8e09abd769124
Hessain Said Hassan <hussayn369@gmail.com>
2026-09-10 15:35:40 +0300

    Fixing Routessss

--- front/src/config/config.js
+++ front/src/config/config.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || "https://qalam.hussayn369.workers.dev/";
+const API_URL = process.env.REACT_APP_API_URL || "https://qalam.velixir.run/api/v1";
 const API_BASE_URL = API_URL.replace(/\/api\/v1\/?$/, "");
 const config = { API_BASE_URL, API_URL };
 
