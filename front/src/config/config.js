ef909f4758ea17d1ee7cbc665b53df47ddf33792
444hussayn444 <hussayn369@gamil.com>
2026-09-10 17:39:17 +0300

    update-everythingg

--- front/src/config/config.js
+++ front/src/config/config.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.REACT_APP_API_URL ||
-  "https://qalam-backend.444hussayn444.deno.net";
+  "https://qalam-backend.444hussayn444.deno.net/api/v1";
 const API_BASE_URL = API_URL.replace(/\/api\/v1\/?$/, "");
 const config = { API_BASE_URL, API_URL };
 
