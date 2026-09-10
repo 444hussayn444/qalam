ccfa59aa97538ecca088ede18f4ed57569cf0549
Hessain Said Hassan <hussayn369@gmail.com>
2026-09-10 15:32:45 +0300

    fixing routing too

--- front/src/config/config.js
+++ front/src/config/config.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api/v1";
+const API_URL = process.env.REACT_APP_API_URL || "https://qalam.hussayn369.workers.dev/";
 const API_BASE_URL = API_URL.replace(/\/api\/v1\/?$/, "");
 const config = { API_BASE_URL, API_URL };
 
